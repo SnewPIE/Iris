4b49ef4c7c6e4b35dafcc4b3bd193c7f7caf8bb7
wlsdnr <74346142+wlsdnr@users.noreply.github.com>
2024-09-03 14:14:03 +0900

    점프 횟수 제한, 애니메이터 생성
    무한점프 막음. 애니메이션으로 움직이기.
    벽점프가 남아있으니 고쳐야함.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,8 +7,11 @@ public class PlayerControl : MonoBehaviour
 {
     float playerSpeed; //플레이어 속도
     float playerJumpHeight; //플레이어 점프높이
+    int playerJumpChance; //플레이어 최대 점프 횟수
+    int JumpCount;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +22,11 @@ public class PlayerControl : MonoBehaviour
     {
         playerSpeed = 5.0f;
         playerJumpHeight = 8.0f;
+        playerJumpChance = 2;
+        JumpCount = 0;
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,10 +39,20 @@ public class PlayerControl : MonoBehaviour
         }
 
         //플레이어 점프 구현
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space)&& playerJumpChance>JumpCount)
         {
             rigid.velocity = new Vector2(rigid.velocity.x, playerJumpHeight);
             //rigid.AddForce(Vector2.up*playerJumpHeight,ForceMode2D.Impulse);
+            JumpCount++;
+        }
+
+        if(rigid.velocity.x == 0)
+        {
+            animator.SetBool("isWalk",false);
+        }
+        else
+        {
+            animator.SetBool("isWalk", true);
         }
     }
 
@@ -46,5 +62,13 @@ public class PlayerControl : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         //입력 방향으로 플레이어 스피드로 이동
         rigid.velocity= new Vector2 (h*playerSpeed, rigid.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            JumpCount = 0;
+        }
     }
 }
